de666061966d053b36ec6706665ecca0b544f9fa
Dora <150006855+dora8008@users.noreply.github.com>
2025-11-02 12:25:53 +0530

    Create CountEnOinArr.cpp

---
+++ CountEnOinArr.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, even = 0, odd = 0;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        if (arr[i] % 2 == 0) even++;
+        else odd++;
+    }
+    cout << "Even: " << even << "\nOdd: " << odd;
+    return 0;
+}
